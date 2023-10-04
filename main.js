814c6de9c64955ffd6cf78a38bbfcd377cef54a0
dvinferno <72324421+dvinferno@users.noreply.github.com>
2023-10-04 17:48:15 -0400

    added getWinner Function

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 /*----- constants -----*/
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 /*----- app's state (variables) -----*/
 let board;
 let turn = "X";
+let win;
 
 /*----- cached element references -----*/
 const squares = Array.from(document.querySelectorAll("#board div"));
+const messages = document.querySelector("h2");
 
 /*----- event listeners -----*/
 document.getElementById("board").addEventListener("click", handleTurn);
@@ -14,6 +27,25 @@ function render() {
   board.forEach(function (mark, index) {
     squares[index].textContent = mark;
   });
+
+  messages.textContent = `It's ${turn}'s turn!`;
+}
+
+function getWinner() {
+  let winner = null;
+
+  winningCombos.forEach(function (combo, index) {
+    if (
+      board[combo[0]] &&
+      board[combo[0]] === board[combo[1]] &&
+      board[combo[0]] === board[combo[2]]
+    )
+      winner = board[combo[0]];
+  });
+  
+  messages.textContent = win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
+
+  return winner;
 }
 
 function handleTurn(event) {
@@ -22,7 +54,9 @@ function handleTurn(event) {
   });
 
   board[index] = turn;
-  console.log(board);
+  win = getWinner();
+  turn = turn === "X" ? "O" : "X";
+  render();
 }
 
 function init() {
