9c25068b5eb076fd84f7824cffc5cfd719726799
dvinferno <72324421+dvinferno@users.noreply.github.com>
2023-10-04 17:32:58 -0400

    javascript setup

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
-console.log('Your JS is linked up. Be the person you needed when you were little.')
+/*----- constants -----*/
+/*----- app's state (variables) -----*/
+let board;
+
+/*----- cached element references -----*/
+const squares = Array.from(document.querySelectorAll('#board div'));
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
+function render() {
+  board.forEach(function (mark, index) {
+    squares[index].textContent = mark;
+  });
+}
+
+function init() {
+  board = ["", "", "", "", "", "", "", "", ""];
+  render();
+}
+
+init();
