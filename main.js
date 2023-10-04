88530888a4a9873e038d126277c22dfc5afe36bd
dvinferno <72324421+dvinferno@users.noreply.github.com>
2023-10-04 17:37:32 -0400

    added handleTurn function

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 /*----- constants -----*/
 /*----- app's state (variables) -----*/
 let board;
+let turn = "X";
 
 /*----- cached element references -----*/
-const squares = Array.from(document.querySelectorAll('#board div'));
+const squares = Array.from(document.querySelectorAll("#board div"));
 
 /*----- event listeners -----*/
+document.getElementById("board").addEventListener("click", handleTurn);
 
 /*----- functions -----*/
 function render() {
   board.forEach(function (mark, index) {
     squares[index].textContent = mark;
   });
+}
+
+function handleTurn(event) {
+  let index = squares.findIndex(function (square) {
+    return square === event.target;
+  });
+
+  board[index] = turn;
+  console.log(board);
 }
 
 function init() {
