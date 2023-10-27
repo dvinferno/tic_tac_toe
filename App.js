167961f2f84879e6e8d888037a4f497b834f29f8
dvinferno <72324421+dvinferno@users.noreply.github.com>
2023-10-27 16:07:19 -0400

    Updated our game to use JSX

--- App.js
+++ App.js
@@ -1,9 +1,25 @@
 function App() {
-    return (
-        <div>
-            <h1>Tic-React-Toe</h1>
+  return (
+    <div>
+      <h1>Tic-React-Toe</h1>
+      <h2>It's X's turn!</h2>
+
+      <div class="flex-container flex-column">
+        <div class="flex-container flex-wrap" id="board">
+          <div class="square"></div>
+          <div class="square"></div>
+          <div class="square"></div>
+          <div class="square"></div>
+          <div class="square"></div>
+          <div class="square"></div>
+          <div class="square"></div>
+          <div class="square"></div>
+          <div class="square"></div>
         </div>
-    )
+        <button id="reset-button">Reset</button>
+      </div>
+    </div>
+  );
 }
 
-ReactDOM.render(<App />, root)
+ReactDOM.render(<App />, root);
